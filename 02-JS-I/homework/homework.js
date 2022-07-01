5c1221be8d6350f7b9069c955b7ae93b1e4dae97
JAVINOBLE10 <javinoble.jmn@gmail.com>
2022-07-01 13:35:31 -0300

    Primera commit 03 JS2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -193,13 +193,13 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return (nombre + "!")
+  return `hola ${nombre} !`
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return (alto * ancho)
+  return (alto * ancho) /2
 }
 
 
@@ -231,7 +231,11 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-
+if letra.length > 1 return "Dato incorrecto"
+  if  (letra=== a|| letra=== e || letra === i || letra===o || letra === u )
+return "Es vocal"
+else if (letra )
+else "Dato incorrecto"
 }
 
 
